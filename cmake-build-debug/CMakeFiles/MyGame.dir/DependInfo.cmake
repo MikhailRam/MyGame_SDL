8086ebc2a185739d2da7b2fcfb3c542c1da435d6
Mikhail Ramzhaev <mikhailramzhaev@Noutbuk-Mikhail.local>
2023-07-11 14:00:26 +0300

    Cmake done. Implementation of SDL2 completed. Start window with moves tracking created

---
+++ cmake-build-debug/CMakeFiles/MyGame.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/mikhailramzhaev/Documents/Works/Programming/CLion_Projects/MyGame/main.cpp" "/Users/mikhailramzhaev/Documents/Works/Programming/CLion_Projects/MyGame/cmake-build-debug/CMakeFiles/MyGame.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../MyGame"
+  "/Library/Frameworks/SDL2.framework/Headers"
+  "/Library/Frameworks/SDL2.framework"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
